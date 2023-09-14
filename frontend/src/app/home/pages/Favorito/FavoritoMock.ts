fb582b42cac4110a8687fcb6bd3d7a60c74c4dc5
uchoagab <86251594+uchoagab@users.noreply.github.com>
2023-09-14 11:38:53 -0300

    Frontend Favoritos

---
+++ frontend/src/app/home/pages/Favorito/FavoritoMock.ts
@@ -0,0 +1,19 @@
+const mockHistoryData = [
+    {
+        user : "UserMock",
+        name : "Resort Muro Alto",
+        id: "a07f7e00",
+    },
+    {
+        user : "UserMock",
+        name : "Porto Seguro Hotel",
+        id : "570f9ae3",
+    },
+    {
+        user : "UserMock",
+        name : "Resort Muro Alto",
+        id : "7565f3",
+    },
+
+    ]
+  export default mockHistoryData;
